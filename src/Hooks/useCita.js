914b85cb69733660d5cd7366b55959e74450f08e
EdgarFuentes100 <edgarfuentes139@gmail.com>
2026-07-07 13:46:16 -0600

    use callback

--- src/Hooks/useCita.js
+++ src/Hooks/useCita.js
@@ -1,28 +1,31 @@
-import { useState } from 'react';
-import { useFetch } from '../api/useFetch';
+import { useState, useCallback } from "react";
+import { useFetch } from "../api/useFetch";
 
 const useCita = () => {
     const { getFetch } = useFetch();
     const [cita, seCita] = useState([]);
 
-    const getCita = (idUsuario) => {
-    console.log(idUsuario);
+    const getCita = useCallback((idUsuario) => {
+        console.log(idUsuario);
+
         const urlParcial = `cita/obtenerLista/${idUsuario}`;
+
         getFetch(urlParcial)
             .then((data) => {
                 const { datos, mensaje, ok } = data;
 
                 if (ok) {
                     seCita(datos);
-                    console.log("cita ", datos);
+                    console.log("cita", datos);
                 } else {
                     console.error(mensaje);
                 }
             })
             .catch((error) => {
-                console.error("Error al obtener doctores:", error);
+                console.error("Error al obtener citas:", error);
             });
-    };
+
+    }, [getFetch]);
 
     return {
         cita,
